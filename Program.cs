c6954f2a751eb8358d1f6df270d306525f9e2236
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2025-08-07 16:32:31 +0100

    Implement cat-file command

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.IO.Compression;
+using System.Text;
+
 if (args.Length < 1)
 {
     Console.WriteLine("Please provide a command");
@@ -14,6 +16,20 @@ if (command == "init")
     Directory.CreateDirectory(".git/refs");
     File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
     Console.WriteLine("Initialized git directory");
+}
+else if (command == "cat-file")
+{
+    if (args.Length < 3 || args[1] != "-p")
+    {
+        Console.WriteLine("Please provide a file");
+        return;
+    }
+
+    string hash = args[2];
+    FileStream fStream = new($".git/objects/{hash[..2]}/{hash[2..]}", FileMode.Open, FileAccess.Read);
+    ZLibStream zlStream = new(fStream, CompressionMode.Decompress);
+    StreamReader reader = new(zlStream, Encoding.UTF8);
+    Console.Write(reader.ReadToEnd().Split("\0")[1]);
 }
 else
 {
