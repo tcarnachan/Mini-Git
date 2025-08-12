157a3f3361df6a63139cad1086534c0f81b91dec
tcarnachan <63309989+tcarnachan@users.noreply.github.com>
2025-08-12 18:40:05 +0100

    Fix ParseTree bug where it failed if there were 'null bytes' in the sha1 hash

--- src/GitObjects/Tree.cs
+++ src/GitObjects/Tree.cs
@@ -20,16 +20,28 @@ namespace GitObjects
         // Tree format: tree <size>\0<mode> <name>\0<20_byte_sha><mode> <name>\0<20_byte_sha>
         private void ParseTree()
         {
-            string[] split = GetString().Split('\0');
-            if (split.Length > 1)
+            int split = Array.IndexOf(content, (byte)0);
+            string[] treeSize = Encoding.UTF8.GetString(content[..split]).Split();
+            if (treeSize[0] != "tree") throw new InvalidOperationException($"Not a tree: {hash}");
+            // Get the size
+            int tmp;
+            if (!int.TryParse(treeSize[1], out tmp)) throw new InvalidFormatException($"Invalid size {treeSize[1]} for {hash}");
+            size = tmp;
+
+            byte[] entries = content[(split+1)..];
+            while (entries.Length > 0)
             {
-                // TODO: mode + hash
-                entries.Add(new TreeEntry("", split[1].Split()[1], ""));
-                for (int i = 2; i < split.Length; i++)
-                {
-                    string[] remain = string.Join("", split[i].Skip(20).ToArray()).Split(); // remain[0] is mode, remain[1] is name
-                    if (remain.Length > 1) entries.Add(new TreeEntry("", remain[1], ""));
-                }
+                // Get until next null byte
+                split = Array.IndexOf(entries, (byte)0);
+                string[] modeName = Encoding.UTF8.GetString(entries[..split]).Split();
+                entries = entries[(split + 1)..]; // +1 to skip the null byte
+
+                // Get hash
+                string hash = Convert.ToHexStringLower(entries[..20]);
+                entries = entries[20..];
+
+                // Add to list
+                this.entries.Add(new TreeEntry(modeName[0], modeName[1], hash));
             }
         }
 
